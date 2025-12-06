acab690919a6de755e51561459040296f9b496dc
kkzdq <63237996+kkzdq@users.noreply.github.com>
2025-12-06 15:03:05 +0800

    Remove commented site record code from Footer.vue
    Removed commented site record section from Footer.vue.

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -25,14 +25,14 @@
             {{ config.efua }}
           </a>
         </span>
-        <!-- 站点备案
+        <!-- 站点备案 -->
         <span>
           <span v-if="siteIcp">
             &amp;&nbsp;
             <a v-if="siteIcp" href="https://beian.miit.gov.cn" target="_blank">
               {{ siteIcp }}
             </a>
-          </span> -->
+          </span>
           <!-- 这备那备的真的很扫（bushi） -->
           <span v-if="siteMps">
             &amp;&nbsp;
