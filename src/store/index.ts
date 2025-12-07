ecc3bc7393b3bf2350fa51fd1204996cd35e0cf7
kkzdq <63237996+kkzdq@users.noreply.github.com>
2025-12-07 15:02:20 +0800

    Update index.ts

--- src/store/index.ts
+++ src/store/index.ts
@@ -9,7 +9,7 @@ export const storeState: MainState = {
   coverType: 1 as number, // 【开关】壁纸种类
   sBGCount: null as string | null, // 【状态】使用内置壁纸时用于临时指定壁纸的接口
   seasonalEffects: true, // 【开关】季节特效
-  msgNameShow: true, // 【开关】信息区域显示自定义名而非原本的 URL
+  msgNameShow: false, // 【开关】信息区域显示自定义名而非原本的 URL
   siteStartShow: true, // 【开关】建站日期显示
   musicClick: true, // 【开关】音乐链接是否跳转
   musicBoxOpenState: false, // 【状态】音乐盒子开启状态
