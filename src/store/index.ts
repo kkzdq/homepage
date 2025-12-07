a797deb77da67f4a419d6468ea12314820977b91
kkzdq <63237996+kkzdq@users.noreply.github.com>
2025-12-07 09:12:09 +0800

    Update index.ts

--- src/store/index.ts
+++ src/store/index.ts
@@ -6,7 +6,7 @@ export const storeState: MainState = {
   // 这些变量，非有能力的开发者请只操作【开关】项来实现个性化的默认设置，其余变量勿动！
   imgLoadStatus: false, // 【状态】壁纸加载状态
   innerWidth: null as number | null, // 【状态】当前窗口宽度
-  coverType: 1 as number, // 【开关】壁纸种类0，1，2，3
+  coverType: 0 as number, // 【开关】壁纸种类0，1，2，3
   sBGCount: null as string | null, // 【状态】使用内置壁纸时用于临时指定壁纸的接口
   seasonalEffects: true, // 【开关】季节特效
   msgNameShow: false, // 【开关】信息区域显示自定义名而非原本的 URL
@@ -14,7 +14,7 @@ export const storeState: MainState = {
   musicClick: true, // 【开关】音乐链接是否跳转
   musicBoxOpenState: false, // 【状态】音乐盒子开启状态
   musicIsOk: false, // 【状态】音乐是否加载完成
-  musicVolume: 0.5 as number, // 【开关】音乐音量
+  musicVolume: 0.7 as number, // 【开关】音乐音量
   musicOpenState: false, // 【状态】音乐面板开启状态
   backgroundShow: false, // 【状态】壁纸展示状态
   boxOpenState: false, // 【状态】盒子开启状态
